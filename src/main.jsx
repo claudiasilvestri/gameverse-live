14b30d8619412a01df36ce104e9e0448446721bf
Claudia <cl.silvestri@yahoo.com>
2026-02-26 16:20:27 +0100

    fix layout path case

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { createRoot } from "react-dom/client";
-import "./Layout/Global.css";
-import "./Layout/Header.css";
-import "./Layout/BackButton.css";
-import "./Layout/Spinner.css";
+import "./layout/Global.css";
+import "./layout/Header.css";
+import "./layout/BackButton.css";
+import "./layout/Spinner.css";
 import Root from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
